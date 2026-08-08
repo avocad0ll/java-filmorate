c6d8e92ffc66fd8557abd3a74c7ae79b7306f92e
polskilew <jorikgrsskom218@gmail.com>
2026-08-08 09:52:35 +0300

    fixes

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -4,7 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.GenreStorage;
+import ru.yandex.practicum.filmorate.storage.MpaStorage;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.util.List;
@@ -14,19 +17,40 @@ import java.util.List;
 public class FilmService {
 	private final FilmStorage filmStorage;
 	private final UserStorage userStorage;
+	private final GenreStorage genreStorage;
+	private final MpaStorage mpaStorage;
 
 	public FilmService(@Qualifier("filmDbStorage") final FilmStorage filmStorage,
-					   @Qualifier("userDbStorage") final UserStorage userStorage) {
+					   @Qualifier("userDbStorage") final UserStorage userStorage,
+					   final GenreStorage genreStorage,
+					   final MpaStorage mpaStorage) {
 		this.filmStorage = filmStorage;
 		this.userStorage = userStorage;
+		this.genreStorage = genreStorage;
+		this.mpaStorage = mpaStorage;
 	}
 
 	public Film add(final Film film) {
+		validateReferences(film);
 		return filmStorage.add(film);
 	}
 
 	public Film update(final Film film) {
+		validateReferences(film);
 		return filmStorage.update(film);
+	}
+
+	private void validateReferences(final Film film) {
+		if (film.getMpa() != null) {
+			mpaStorage.getById(film.getMpa().getId());
+		}
+		if (film.getGenres() != null) {
+			for (Genre genre : film.getGenres()) {
+				if (genre != null) {
+					genreStorage.getById(genre.getId());
+				}
+			}
+		}
 	}
 
 	public Film getById(final int id) {
